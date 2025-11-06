9fd3a6dc61832665eba82b6481f603f112141bf9
zacheen <zacheenoao@gmail.com>
2025-11-05 19:39:51 -0800

    backend after optimization (not tested yet)

---
+++ Algorithm.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+class UF_by_size:
+    def __init__(self):
+        self.id = {}
+        self.set_member_cnt = defaultdict(lambda : 1)
+
+    def check_same_union(self, u, v):
+        i = self.find(u)
+        j = self.find(v)
+        return i == j
+
+    def union(self, u, v):
+        i = self.find(u)
+        j = self.find(v)
+        if i == j:
+            return
+        # to minimize the adjust of find(), I union the smaller set into the bigger set
+        if self.set_member_cnt[i] > self.set_member_cnt[j] :
+            i,j = j,i
+        self.set_member_cnt[j] += self.set_member_cnt[i]
+        del(self.set_member_cnt[i])
+        self.id[i] = j
+
+    def find(self, up):
+        while up in self.id and up != (deep := self.id[up]):
+            self.id[up] = up = (self.id[deep] if deep in self.id else deep)
+        return up
+    
+from heapq import heappush, heappop
+from math import inf
+# defaultdict version, so that we can add in any amount and any type of item
+class Bidirectional_Dijkstra:
+    def __init__(self):
+        self.adj_matrix = defaultdict(lambda : defaultdict(lambda : inf))
+
+    def add_edge(self, n1, n2, weight):
+        self.adj_matrix[n1][n2] = weight
+        self.adj_matrix[n2][n1] = weight
+
+    # check having any friends (if no friends, it is impossible to expand friendship)
+    def check_data(self, people): 
+        return len(self.adj_matrix[people]) > 0
+    
+    # return another direction
+    # dir: 0 - Forward, 1 - Backward
+    def other_dir(self, dir):
+        return 1-dir
+
+    def find_min_path(self, start_fri, target_fri):
+        # Handle same person case
+        if start_fri == target_fri:
+            return (0, [start_fri])
+
+        # using list to memorize both directions' info
+        dists = [{start_fri: 0}, {target_fri: 0}]  # total distances from start and target node
+        paths = [{start_fri: [start_fri]}, {target_fri: [target_fri]}]  # minimum weight paths to start and target node
+        node_heap = [[(0, start_fri)], [(0, target_fri)]]  # heap of (distance, node) for choosing next node to expand
+
+        min_dist = inf
+        min_path = []
+        dir = 1
+        while node_heap[0] and node_heap[1]:
+            # choose direction
+            # dir == 0 is forward direction and dir == 1 is back
+            dir = self.other_dir(dir)
+            # get minimum distance to expand
+            now_dist, now_fri = heappop(node_heap[dir])
+            if now_dist > dists[dir][now_fri]:
+                # Shortest path to now_fri has already been found
+                continue
+            # update distance
+            dists[dir][now_fri] = now_dist  # equal to seen[dir][v]
+            if now_fri in dists[self.other_dir(dir)]:
+                # if we have scanned v in both directions we are done
+                # we have now discovered the shortest path
+                return (min_dist, min_path)
+
+            for new_fri, new_weight in self.adj_matrix[now_fri].items():
+                new_dist = now_dist + new_weight
+                if new_fri not in dists[dir] or new_dist < dists[dir][new_fri]:
+                    dists[dir][new_fri] = new_dist
+                    heappush(node_heap[dir], (new_dist, new_fri))
+                    paths[dir][new_fri] = paths[dir][now_fri] + [new_fri]
+                    if new_fri in dists[0] and new_fri in dists[1]: # if new_fri connects both sets
+                        # check whether this path is better
+                        totaldist = dists[0][new_fri] + dists[1][new_fri]
+                        if min_dist > totaldist:
+                            min_dist = totaldist
+                            min_path = paths[0][new_fri][:-1] + paths[1][new_fri][::-1]
+                            # since paths[0][new_fri][-1] == paths[1][new_fri][-1]
+        return (None, None)
---
+++ Backend.py
@@ -0,0 +1,40 @@
+from Algorithm import *
+
+class Backend:
+    def init_space(self):
+        self.uf = UF_by_size()
+        self.graph = Bidirectional_Dijkstra()
+    
+    def __init__(self, path):
+        self.init_space()
+        with open(path, "r") as fr :
+            while fr :
+                # deal with data
+                input_line = fr.readline()
+                if input_line == "-\n" :
+                    continue
+                if input_line == "\n" or input_line == "":
+                    break
+
+                fri1, fri2, score = input_line.split(", ")
+                score = int(score[:-1])
+                self.add_relation(fri1, fri2, score)
+
+    # def __init__(self, relation):
+    #     self.init_space()
+    #     for fri1, fri2, score in relation :
+    #         self.add_relation(fri1, fri2, score)
+
+    def add_relation(self, fri1, fri2, connection_score):
+        self.uf.union(fri1, fri2)
+        self.graph.add_edge(fri1, fri2, connection_score)
+
+    def check_relation(self, fri1, fri2):
+        if self.graph.check_data(fri1) and self.graph.check_data(fri2):
+            return self.uf.check_same_union(fri1, fri2)
+        return False
+
+    def get_best_path(self, fri1, fri2):
+        if not self.check_relation(fri1, fri2) :
+            return (None, None)
+        return self.graph.find_min_path(fri1, fri2)
